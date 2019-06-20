ffb2bf744b103254bd3eabfed02a84475fe270d7
WuShuang <1306195411@qq.com>
2019-06-20 11:44:17 +0800

    first commit

---
+++ src/main/java/com/example/demo/controller/Test.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+
+    @RequestMapping(value="/demo")
+    public String demo() {
+        return "demo";
+    }
+
+}
+
