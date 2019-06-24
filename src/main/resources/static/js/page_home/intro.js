a7d19359d62fa380b3da22b915c87654073b4af1
wanwenxin <631319789@qq.com>
2019-06-24 18:32:41 +0800

    this is a modify wenjian

--- src/main/resources/static/js/page_home/intro.js
+++ src/main/resources/static/js/page_home/intro.js
@@ -1,20 +1,24 @@
 jQuery(document).ready(function($){
     var $right_arrow = $('.triangle').eq(0),
-        $left_arrow = $('.triangle').eq(1);
-        $intro_pic1 = $('.intro_pic').eq(0);
-        $intro_pic2 = $('.intro_pic').eq(1);
-        $intro_pic3 = $('.intro_pic').eq(2);
+        $left_arrow = $('.triangle').eq(1),
+        $intro_pic1 = $('.intro_pic').eq(0),
+        $intro_pic2 = $('.intro_pic').eq(1),
+        $intro_pic3 = $('.intro_pic').eq(2),
+        Timer;
         intro_picArr = new Array($intro_pic1,$intro_pic2,$intro_pic3)
         pos = 2;
     $right_arrow.on('click', function(event){
-      console.log((pos+1)%3);
       intro_picArr[pos].animate({opacity: '0'},1000);
+      intro_picArr[pos].css('z-index','0');
       pos = (pos+1)%3;
       intro_picArr[pos].animate({opacity: '1'},1000);
+      intro_picArr[pos].css('z-index','1');
     });
     $left_arrow.on('click', function(event){
       intro_picArr[pos].animate({opacity: '0'},1000);
+      intro_picArr[pos].css('z-index','0');
       pos = (pos-1+3)%3;
       intro_picArr[pos].animate({opacity: '1'},1000);
+      intro_picArr[pos].css('z-index','1');
     });
 });
