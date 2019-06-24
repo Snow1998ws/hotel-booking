91e42d185e50d2a6318f76ad5e32fce92cc56ce5
thesakura <1165597739@qq.com>
2019-06-24 12:06:39 +0800

    second

--- src/main/java/com/example/demo/controller/Test.java
+++ src/main/java/com/example/demo/controller/Test.java
@@ -1,18 +1,47 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+import java.util.Map;
+
+@Controller
+@RequestMapping("")
 public class Test {
-    @RequestMapping(value = "/demo")
-    public String demo() {
-        return "demo";
+
+    @Autowired
+
+    @GetMapping("/")
+    public String loginpage()
+    {
+        return "redirect:/login.html";
     }
+
+    @RequestMapping("/home")
+    public String Tohome()
+    {
+        return "home.html";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("id") String username,
+                        @RequestParam("psd") String password,
+                        Map<String, Object> map){
+
+        if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+
+            return "redirect:home";
+        }
+
+        map.put("msg", "用户名密码错误");
+        return "redirect:login.html#0";
+    }
+
 }
 
