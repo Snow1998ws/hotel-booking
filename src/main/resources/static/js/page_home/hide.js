fc15073abbcb29ecefdb1258f7c80c096909917c
wanwenxin <631319789@qq.com>
2019-06-26 17:49:13 +0800

    beautify pic change UI

--- src/main/resources/static/js/page_home/hide.js
+++ src/main/resources/static/js/page_home/hide.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
     var $tab_log = $('.Titem2').children('a'),
         $part_log = $('.log');
-        $cancel = $('.cancel');
+        $cancel = $('.icon_cancel');
 	  $tab_log.on('click', function(event){
 		  $part_log.animate({left:'0'},1000);
     });
