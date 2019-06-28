957092a7c4a4be773f2347625f4292077767a854
WuShuang <1306195411@qq.com>
2019-06-28 09:25:35 +0800

    add search by checkin and leave date

--- src/main/java/com/example/demo/service/HotelService.java
+++ src/main/java/com/example/demo/service/HotelService.java
@@ -3,11 +3,14 @@ package com.example.demo.service;
 import com.example.demo.domain.Hotel;
 import com.example.demo.domain.Room;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 import java.util.List;
 
 @Service
 public interface HotelService {
     List<Hotel> findHotelSelective(Hotel hotel);
+    List<Hotel> findHotelByDateAndCity(String city, Date checkin_time, Date leave_time);
     List<Hotel> findHotel();
     Hotel findHotelById(int id);
     List<Hotel> findHotelsByRooms(List<Room> rooms);
--- src/main/java/com/example/demo/service/HotelServiceImpl.java
+++ src/main/java/com/example/demo/service/HotelServiceImpl.java
@@ -4,10 +4,13 @@ import com.example.demo.domain.Hotel;
 import com.example.demo.domain.HotelExample;
 import com.example.demo.domain.Room;
 import com.example.demo.mapper.HotelMapper;
+import com.example.demo.mapper.OrdersMapper;
+import com.example.demo.mapper.RoomMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,10 +18,19 @@ public class HotelServiceImpl implements HotelService{
 
     @Autowired
     private HotelMapper hotelMapper;
+    @Autowired
+    private RoomMapper roomMapper;
+    @Autowired
+    private OrdersMapper ordersMapper;
 
     @Override
     public List<Hotel> findHotel() {
         return hotelMapper.selectByExample(new HotelExample());
+    }
+
+    @Override
+    public List<Hotel> findHotelByDateAndCity(String city, Date checkin_time, Date leave_time) {
+        return hotelMapper.selectByDateAndCity(city, checkin_time, leave_time);
     }
 
     @Override
