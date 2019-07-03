577d24a44c61f811fd875ae36968fb7ad74f1066
thesakura <1165597739@qq.com>
2019-07-03 19:24:20 +0800

    add reset function

---
+++ src/main/resources/static/js/admin/adminHotelOp.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+    $op_button = $('#clear');
+    $op_button.on("click", clear);
+});
+
+function clear() {
+    $('.check_input').val("");
+    $('.check_textarea').val("");
+}
