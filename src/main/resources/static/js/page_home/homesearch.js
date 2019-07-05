64831d05f5e3ec1389f17f038aad3b720f1f25a8
thesakura <1165597739@qq.com>
2019-07-05 11:44:08 +0800

    modify

---
+++ src/main/resources/static/js/page_home/homesearch.js
@@ -0,0 +1,11 @@
+$(document).ready(function()
+{
+    $titleSearch=$('.title_search');
+    alert("1234");
+    $titleSearch.keydown(function (event) {
+        if(event.keyCode == 13){
+            
+            alert("123")
+        }
+    })
+});
